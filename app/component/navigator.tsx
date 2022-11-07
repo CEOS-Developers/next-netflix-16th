9a6ea9a050cb75b1a59f94d096f2b5afa76f96b6
Jieun Oh <0909oje@naver.com>
2022-11-07 23:01:18 +0900

    feat : 네비게이터 png 추가

---
+++ app/component/navigator.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css'
+import styled from "styled-components";
+
+import navi1
+
+
+const Navigator = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
+
+export default Navigator;
