8a26c5847b6356811e786d268ee23b46c2fa254a
Jieun Oh <0909oje@naver.com>
2022-11-07 23:06:08 +0900

    fix : 네비게이터 작업중

--- app/component/navigator.tsx
+++ app/component/navigator.tsx
@@ -1,8 +1,11 @@
 import styles from '../styles/Home.module.css'
 import styled from "styled-components";
 
-import navi1
-
+import navi1 from '../asset/navigator/navi1.png';
+import navi2 from '../asset/navigator/navi2.png';
+import navi3 from '../asset/navigator/navi3.png';
+import navi4 from '../asset/navigator/navi4.png';
+import navi5 from '../asset/navigator/navi5.png';
 
 const Navigator = () => {
   return (
