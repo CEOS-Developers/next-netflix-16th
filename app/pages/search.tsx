6ad82f8c1c61fd58daa266b1b72518fdbf0b131b
Chaeyeon An <dkscodus1204@ewhain.net>
2022-11-09 23:15:14 +0900

    feat: 검색 기능 완료

--- app/pages/search.tsx
+++ app/pages/search.tsx
@@ -7,26 +7,17 @@ import search from "../asset/icons/searchIcon.svg";
 import close from "../asset/icons/close.svg";
 import api from "../asset/api";
 import apiKey from "../asset/apiKey";
-import imgPath from "../asset/imgPath";
 import { useEffect, useState } from "react";
+import useInput from "../component/hooks/useInput";
 
 export default function Home() {
   const [info, setInfo] = useState([] as any);
+  const { text, handleChange, resetText } = useInput("");
 
   useEffect(() => {
-    const movie_id = 550;
-    // https://developers.themoviedb.org/3/movies/get-movie-images : 영화이미지
-    // fetch(`${api}/movie/${movie_id}/images?api_key=${apiKey}`)
-    // fetch(`${api}/movie/${movie_id}?api_key=${apiKey}`)
-    // fetch(`${api}/movie/latest?api_key=${apiKey}`)
-
-    // 최신 영화 이미지 리스트 나옴. 영화 19개
-    // fetch(`${api}/movie/now_playing?api_key=${apiKey}`)
-
     fetch(`${api}/movie/now_playing?api_key=${apiKey}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setInfo(data.results);
       });
   }, []);
@@ -38,17 +29,25 @@ export default function Home() {
           <ImgWrap>
             <Image src={search} alt="search" />
           </ImgWrap>
-          <Input type="text" placeholder="Search for movies" />
-          <ImgWrap>
+          <Input
+            value={text}
+            onChange={handleChange}
+            placeholder="Search for movies"
+          />
+          <ImgWrap onClick={resetText}>
             <Image src={close} alt="close" />
           </ImgWrap>
         </Wrap>
 
         <div>
           <Title>Top Searches</Title>
-          {info.map((movie: any) => (
-            <SearchItem name={movie.title} imgSrc={movie.backdrop_path} />
-          ))}
+          {info
+            .filter((i: any) =>
+              i.title.toLowerCase().includes(text.toLowerCase())
+            )
+            .map((movie: any) => (
+              <SearchItem name={movie.title} imgSrc={movie.backdrop_path} />
+            ))}
         </div>
 
         <Navigator />
@@ -68,8 +67,11 @@ const Wrap = styled.div`
 `;
 
 const ImgWrap = styled.div`
-  width: 18px;
-  height: 18px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.div`
