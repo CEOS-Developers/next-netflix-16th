a9e3a6ab20440284777ebfee0d67d2393550d4a8
YoommY <isho48239@gmail.com>
2022-11-11 16:59:20 +0900

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Wrapper } from './styles/Wrapper';
-import Navigator from './components/Navigator/navigator';
+import Navigator from './components/Navigators/navigator';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
